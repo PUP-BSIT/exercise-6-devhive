10db1805b2db19faa79a518f40ce41b28a104d25
Mikka Kette <esparagozamikkakette@gmail.com>
2025-03-19 23:24:33 +0800

    add comments to goals

--- esparagoza_mikkakette/script.js
+++ esparagoza_mikkakette/script.js
@@ -1,16 +1,18 @@
-function buttonEnabler() {
-    let nameInput = document.getElementById("commenter-name");
-    let commentText = document.getElementById("commenter-comment");
-    let commentButton = document.getElementById("comment-button");
+document.addEventListener('DOMContentLoaded', function () {
+    const nameInput = document.getElementById('commenter-name');
+    const commentText = document.getElementById('commenter-comment');
+    const commentButton = document.getElementById('comment-button');
 
-    if(nameInput.value.trim() !== '' && commentText.value.trim() !== '') {
-        commentButton.disabled = false;
-    } else {
-        commentButton.disabled = true;
+    function checkFields() {
+        if (nameInput.value.trim() !== '' && commentText.value.trim() !== '') {
+            commentButton.disabled = false;
+        } 
+        
+        else {
+            commentButton.disabled = true;
+        }
     }
-    
-    nameInput.addEventListener('input', buttonEnabler);
-    commentText.addEventListener('input', buttonEnabler);
-}
 
-document.addEventListener('DOMContentLoaded', buttonEnabler);
+    nameInput.addEventListener('input', checkFields);
+    commentText.addEventListener('input', checkFields);
+});
