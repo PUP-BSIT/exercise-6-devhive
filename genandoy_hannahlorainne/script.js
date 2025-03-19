3ffe3bc462079b8683eae517de5227b803d7e95c
Hannah Genandoy <lorainneh540@gmail.com>
2025-03-19 21:38:55 +0800

    change js code

--- genandoy_hannahlorainne/script.js
+++ genandoy_hannahlorainne/script.js
@@ -1,12 +1,10 @@
-    document.addEventListener("DOMContentLoaded", function() {
-        const nameInput = document.getElementById('commenter-name');
-        const commentInput = document.getElementById('comment-text');
-        const commentButton = document.getElementById('comment-btn');
+const nameInput = document.getElementById('commenter-name');
+const commentInput = document.getElementById('comment-text');
+const commentButton = document.getElementById('comment-btn');
 
-        function toggleButton() {
-            commentButton.disabled = !(nameInput.value.trim() && commentInput.value.trim());
-        }
+function toggleButton() {
+    commentButton.disabled = !(nameInput.value.trim() && commentInput.value.trim());
+}
 
-        nameInput.addEventListener('input', toggleButton);
-        commentInput.addEventListener('input', toggleButton);
-    });
+nameInput.addEventListener('input', toggleButton);
+commentInput.addEventListener('input', toggleButton);
