21529b1f0ba90bfe30239c6866a842d3bec4f5fc
genandoy-hannahlorainne <lorainneh540@gmail.com>
2025-03-19 18:28:53 +0800

    add disable function in js and css (#8)

--- genandoy_hannahlorainne/script.js
+++ genandoy_hannahlorainne/script.js
@@ -1,10 +1,12 @@
-const nameInput = document.getElementById('commenter-name');
-const commentInput = document.getElementById('comment-text');
-const commentButton = document.getElementById('comment-btn');
+    document.addEventListener("DOMContentLoaded", function() {
+        const nameInput = document.getElementById('commenter-name');
+        const commentInput = document.getElementById('comment-text');
+        const commentButton = document.getElementById('comment-btn');
 
-function toggleButton() {
-    commentButton.disabled = !(nameInput.value.trim() && commentInput.value.trim());
-}
+        function toggleButton() {
+            commentButton.disabled = !(nameInput.value.trim() && commentInput.value.trim());
+        }
 
-nameInput.addEventListener('input', toggleButton);
-commentInput.addEventListener('input', toggleButton);
+        nameInput.addEventListener('input', toggleButton);
+        commentInput.addEventListener('input', toggleButton);
+    });
